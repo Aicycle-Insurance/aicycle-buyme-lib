7f338380835e05c33f35fd7ca9b9fbaf19ca1cfd
tungakanui <tung.ngthanh99@gmail.com>
2023-12-08 11:05:30 +0700

    fix config token

--- lib/aicycle.dart
+++ lib/aicycle.dart
@@ -1,3 +1,4 @@
+import 'package:aicycle_buyme_lib/features/aicycle_buy_me/presentation/aicycle_buy_me.dart';
 import 'package:camera/camera.dart';
 
 import 'features/folder_details/presentation/widgets/controller/folder_detail_controller.dart';
@@ -6,7 +7,8 @@ import 'injection_container.dart';
 export 'features/aicycle_buy_me/presentation/aicycle_buy_me.dart' hide apiToken;
 
 class AICycle {
-  static Future<void> initial() async {
+  static Future<void> initial({String? token}) async {
+    apiToken = token;
     InjectionContainer.initial();
     cameras = await availableCameras();
   }
